aab71c5d17fd9567947293ff05b4c3e6e1b68f4d
irzaorvala <orvala2010@gmail.com>
2022-04-26 17:10:12 +0700

    add components & edit some file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { GoMarkGithub } from "react-icons/go";
+
+import { Container, Topbar, Title, RepoList } from "./styles";
+
+import SearchBar from "./components/SearchBar/SearchBar";
+import Repo from "./components/ListRepo/ListRepo";
+import User from "./components/User/User";
 
 function App() {
+  const [user, setUser] = useState({
+    prof: {},
+    repos: []
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const inputEl = useRef(null);
+
+  async function search() {
+    setLoading(true);
+    const { prof, repos } = await getApiData();
+    setLoading(false);
+
+    if (prof.message === "Not Found") {
+      inputEl.current.focus();
+    } else {
+      setUser({
+        prof,
+        repos
+      });
+    }
+  }
+
+  async function getApiData() {
+    const [prof, repos] = await Promise.all([
+      fetch(
+        `https://api.github.com/users/${inputEl.current.value}`
+      ).then(response => response.json()),
+      fetch(
+        `https://api.github.com/users/${inputEl.current.value}/repos`
+      ).then(response => response.json())
+    ]);
+
+    return { prof, repos };
+  }
+
+  function handleEnterKey() {
+    const ENTER = 13;
+    if (window.event.keyCode === ENTER) {
+      search();
+      inputEl.current.blur();
+    }
+  }
+
+  function selectText() {
+    inputEl.current.select();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <Container>
+      <Topbar>
+        <Title>ListRepos</Title>
+        <SearchBar
+          onKeyPress={handleEnterKey}
+          onFocus={selectText}
+          searchFunction={search}
+          loadingState={loading}
+          inputRef={inputEl}
+        />
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/irzaorvalaa"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <GoMarkGithub size="36px" color="#eee" />
         </a>
-      </header>
-    </div>
+      </Topbar>
+
+      {user.prof.id && (
+        <>
+          <User prof={user.prof} />
+
+          <RepoList>
+            {user.repos.map(r => (
+              <li className="repo" key={r.id}>
+                <Repo repo={r} />
+              </li>
+            ))}
+          </RepoList>
+        </>
+      )}
+    </Container>
   );
 }
 
